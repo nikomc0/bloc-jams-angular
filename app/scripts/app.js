938d1461dcfaa9f3a82ec10f546072e1fe1c8684
Dominick <dominick@theosfp.com>
2017-03-16 14:28:24 -0700

    Checkpoint-3-Routing & States

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,16 +3,20 @@
 		$locationProvider
 			.html5Mode({
 				enabled: true,
-				requireBase: false;
+				requireBase: false
 			});
 		$stateProvider 
 			.state('landing', {
 				url: '/',
 				templateUrl: '/templates/landing.html'
+			})
+			.state('album', {
+				url: '/',
+				templateUrl: '/templates/album.html'
 			});
 	}
 	angular
-		.module('blocJams', ['ui.router']);
+		.module('blocJams', ['ui.router'])
 		.config(config);
 })();
 
