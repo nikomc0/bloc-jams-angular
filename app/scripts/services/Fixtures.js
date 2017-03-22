41663a1b9df091f6a6b0a8ed4e8a6a26ed8659b0
Dominick <dominick@theosfp.com>
2017-03-21 20:07:06 -0700

    Assignment-6, Collection

--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -36,6 +36,16 @@
 			return albumPicasso;
 		};
 		
+		Fixtures.getCollection = function (numberOfAlbums) {
+			this.albums = [];
+			
+			for (var i = 0; i < numberOfAlbums; i++) {
+				this.albums.push(angular.copy(albumPicasso));
+			}
+			
+			return this.albums;
+		};
+		
 		return Fixtures;
 	}
 	
